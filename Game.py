b0d772af13eb88fa328d7d2f22b4745e537710e6
CAUCapstone <yesol7197@gmail.com>
2020-11-27 20:20:56 +0900

    delete not using function

--- Game.py
+++ Game.py
@@ -3,39 +3,12 @@ from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
 
 import sys
-import math
-import os
-import random
 
-# Basic intervals
-from direct.interval.IntervalGlobal import *
-from direct.interval.LerpInterval import *
-
-# Task managers
 from direct.task.Task import Task
-import time
-
-# Audio managers
-from direct.showbase import Audio3DManager
-
-# GUI
-from direct.gui.OnscreenText import OnscreenText
-from direct.gui.OnscreenImage import OnscreenImage
-from panda3d.core import TransparencyAttrib
 from direct.gui.DirectGui import *
-
-# Import External Classes
-from Obj3D import *
-from Racecar import *
 from Racetrack import *
 from Terrain import *
-
-from RacetrackGenerator import *
-
 from CameraController import *
-
-from TabbedFrame import TabbedFrame
-
 from panda3d.core import loadPrcFileData
 
 # Globally change window title name
@@ -44,30 +17,17 @@ loadPrcFileData("", f"window-title {gameTitle}")
 
 class Game(ShowBase):
     fonts = {}
-    selectedTrack = "random.track"
+    selectedTrack = "Hexagon.track"
     selectedCar = "truck_blue"
     selectedPassenger = "bunny"
     level = "medium"
 
     currentState = None
 
-    instructionsText = """\
-[WASD/Arrow Keys] Drive
-[Hold Space] Drift (while driving)
-[1, 2, 3] Change camera view
-[Hold C] Look behind
-[Hold V] Look around
-[P] Pause and show help
-[M] Mute main music and sfx
-[R] Restart Game
-"""
-
     def __init__(self):
         ShowBase.__init__(self)
 
         Game.fonts["Action_Man"] = loader.loadFont('Action_Man.ttf')
-
-        self.helpDialog = HelpDialog()
 
         self.nextState("start")
 
@@ -82,8 +42,6 @@ class Game(ShowBase):
             StartScreen()
         elif state in [ "game", "racing", "racinggame", "main" ]:
             RacingGame()
-        elif state in [ "instructions", "help" ]:
-            InstructionsScreen()
         else:
             print(f"ERROR: State {state} not found")
             sys.exit()
@@ -91,136 +49,21 @@ class Game(ShowBase):
     def destroyInstance(self):
         self.destroy()
 
-class HelpDialog():
-    def __init__(self):
-        self.components = []
-        self.hidden = False
-
-        self.bg = OnscreenImage(
-           image="img/startscreen.jpeg",
-           scale=(1.5, 1.5, 1)
-        )
-
-
-        self.components.append(self.bg)
-
-        # Construct our TabbedFrame
-        self.frame = TabbedFrame(tab_frameSize=(0, 7, 0, 2),
-                                 tab_text_align=TextNode.ALeft,
-                                 tab_text_pos=(0.2, 0.6))
-
-        font = Game.fonts["Action_Man"]
-
-        # Controls
-        page1 = DirectFrame()
-        text = TextNode("text")
-        text.setText("""\
-[WASD/Arrow Keys] Drive
-[Hold Space] Drift (while driving)
-[1, 2, 3] Change camera view
-[Hold C] Look behind
-[Hold V] Look around
-[P] Pause and show help
-[M] Mute main music and sfx
-[R] Restart Game
-""")
-        text.setWordwrap(15)
-        text.setTextColor(0, 0, 0, 1)
-        #text.setFont(font)
-
-        textNP = NodePath(text)
-        textNP.reparentTo(page1)
-        textNP.setScale(0.1)
-        textNP.setPos(-0.7, 0, 0.6)
-
-        page2 = DirectFrame()
-        text = TextNode("text")
-        text.setText("""\
-[3D Models]
-Jeep Model by bigcrazycarboy
-Lightning Bolt by Savino
-All other models from the Alice Gallery
-[Audio]
-Main music adapted from Purple Passion by Diana Boncheva
-[Code Reference]
-Panda 3D Manual
-Panda 3D API Reference
-Panda 3D forums
-""")
-
-        text.setWordwrap(20 )
-        text.setTextColor(0, 0, 0, 1)
-        #text.setFont(font)
-
-        textNP = NodePath(text)
-        textNP.reparentTo(page2)
-        textNP.setScale(0.072 )
-        textNP.setPos(-0.7, 0, 0.7)
-
-        # And finally, add the pages to our TabbedFrame, along with labels
-        # for their buttons
-        self.frame.addPage(page1, "Controls")
-        self.frame.addPage(page2, "Credits")
-
-        self.components += self.frame.pageButtons
-
-        self.components.append(self.frame)
-
-        self.nextButton = DirectButton(
-            text="Next", text_font=Game.fonts["Action_Man"],
-            scale=0.10, command=self.hide,
-            pad=(0.3, 0.3),
-            pos=(0, 0, -0.75),
-            text_mayChange=True
-        )
-        self.components.append(self.nextButton)
-
-        self.buttonHelperText = OnscreenText(
-            text='[P]', pos=(0, -0.87), scale=0.07,
-            font=Game.fonts["Action_Man"],
-            align=TextNode.ACenter, mayChange=True,
-            bg=(182, 182, 182, 0.5),
-        )
-        self.components.append(self.buttonHelperText)
-
-    def show(self):
-        self.hidden = False
-        for component in self.components:
-            component.show()
-
-    def hide(self):
-        self.hidden = True
-        for component in self.components:
-            component.hide()
-
-    def toggleVisible(self):
-        if self.hidden:
-            self.show()
-        else:
-            self.hide()
-
-    def destroy(self):
-        self.destroyed = True
-        for component in self.components:
-            component.destroy()
 
 class StartScreen(Game):
     def __init__(self):
         ShowBase.__init__(self)
-
 
         concreteBg = OnscreenImage(
             image="img/startscreen.jpeg",
             scale=(1.5, 1.5, 1)
         )
 
-
         title = OnscreenText(
             text=gameTitle, pos=(0, 0.5), scale=0.32,
             font=Game.fonts["Action_Man"],
             align=TextNode.ACenter, mayChange=False
         )
-
 
         startGameButton = DirectButton(
             text="Start  Game", text_font=Game.fonts["Action_Man"],
@@ -236,90 +79,6 @@ class StartScreen(Game):
         self.nextState("game")
 
 
-    # Define a procedure to move the camera.
-    def trackShowcase(self, task):
-        rotateTime = 3.0
-        angle = task.time * rotateTime
-
-        rad = 100
-        camHeight = 100
-
-        self.camera.setPos(
-            rad * math.sin(angle), -rad * math.cos(angle), camHeight
-        )
-        self.camera.lookAt(0,0,0)
-        return Task.cont
-
-    def randomiseTrack(self):
-        RacetrackGenerator()
-
-        self.randomText.setText("Random track created!")
-
-        self.menu.set(index=self.tracks.index("random.track"))
-
-        self.selectTrack("random.track")
-
-    def selectTrack(self, track):
-        Game.selectedTrack = track
-
-        points = Racetrack.parseTrackFile(track)
-
-
-        # Camera Control
-        baseVec = LVector3f(0, 20, -3)
-
-
-    def selectCar(self):
-        self.camControl.enabled = False
-        self.nextState("racecar")
-
-    def findTracks(self, path):
-        if os.path.isfile(path):
-            if path.endswith(".track"):
-                return [path.replace("racetracks/", "", 1) ]
-            else:
-                return []
-        elif os.path.isdir(path):
-            tracks = []
-
-            for f in os.listdir(path):
-                tracks += self.findTracks(path + "/" + f)
-
-            return tracks
-        else:
-            return []
-
-    # Define a procedure to move the camera.
-    def carShowcase(self, task):
-        rotateTime = 3.0
-        angle = task.time * rotateTime
-
-        rad = 80
-        camHeight = 10
-
-        self.camera.setPos(
-            rad * math.sin(angle), -rad * math.cos(angle), camHeight
-        )
-        self.camera.setHpr(radToDeg(angle), 0, 0)
-        return Task.cont
-
-    def startGame(self):
-        self.nextState("instructions")
-
-class InstructionsScreen(Game):
-    def __init__(self):
-        ShowBase.__init__(self)
-
-        self.helpDialog = HelpDialog()
-        self.helpDialog.nextButton["command"] = self.startGame
-        self.helpDialog.buttonHelperText.setText("[Space]")
-
-        # Next frame without clicking
-        self.accept("space-up", self.startGame)
-
-    def startGame(self):
-        self.nextState("game")
-
 class RacingGame(Game):
     def __init__(self):
         ShowBase.__init__(self)
@@ -332,9 +91,6 @@ class RacingGame(Game):
         self.gameOverTime = 0 # for camera rotation
         self.printStatements = False
 
-        self.helpDialog = HelpDialog()
-        self.helpDialog.hide()
-        self.helpDialog.nextButton["command"] = self.togglePause
 
         self.totalLaps = 1
 
@@ -346,15 +102,8 @@ class RacingGame(Game):
         # Load collision handlers
         self.collisionSetup(showCollisions=False)
 
-        # Load Music
-        # NOTE: Do before models so that cars can have audio3d object preinitialised
         self.loadAudio()
-
-        # Load the various models
         self.loadModels()
-
-        # Load lights and the fancy background
-        # NOTE: Racetrack needs to be generated first to properly generate the terrain
         self.loadBackground()
         self.loadLights()
 
@@ -366,13 +115,9 @@ class RacingGame(Game):
         self.camConfigDefault = "perspective"
         self.camConfig = self.camConfigDefault
         self.taskMgr.add(self.setCameraToPlayer, "SetCameraToPlayer")
-
-        # Check for key presses
-        # And do corresponding action
         self.taskMgr.add(self.keyPressHandler, "KeyPressHandler")
-
-        # Start a game timer
         self.taskMgr.add(self.gameTimer, "GameTimer")
+
 
     def setCameraToPlayer(self, task):
         # Focus on winning car when gameover
@@ -387,9 +132,6 @@ class RacingGame(Game):
         y += player.offsetY
         z += player.offsetZ
 
-        # Math to make camera always facing player
-        # And at a constant distance camDistance away
-        # Note that cam distance is in the
         camDistance = player.dimY * 1.5
 
         # Allow for variable camera configuration
@@ -402,7 +144,7 @@ class RacingGame(Game):
             theta = (task.time - self.gameOverTime) * 2.5 + degToRad(h)
 
             # Stop rotation after n rotations
-            nRotations = 3
+            nRotations = 1
             if self.isGameOver and theta - degToRad(h) >= ( (nRotations-1) * 2 * math.pi + math.pi):
                 self.setCameraView("perspective_behind_win")
                 self.pauseAudio()
@@ -410,53 +152,18 @@ class RacingGame(Game):
         if "_behind" in self.camConfig:
             theta = degToRad(h - 180)
 
-        # Top-down view
-        if self.camConfig == "birdsEye":
-            xOffset = 0
-            yOffset = 0
-            camHeight = camDistance * 3
-            camDistance = 0
-
-            phi = -90
-
-            self.camera.setHpr(radToDeg(theta), phi, 0)
-        elif self.camConfig == "firstPerson":
-            camDistance = self.player.passenger.offsetY + self.player.passenger.dimY
-            camDistance *= -1
-
-            camHeight = self.player.passenger.offsetZ + self.player.dimZ/2
-
-            phi = -10
-
-            self.camera.setHpr(radToDeg(theta), phi, 0)
-        # Camera has a slight tilt
-        else:
-            # Fix camera angle according to car model
-            carName = self.player.modelName.replace("car_", "", 1)
-
-            if carName == "jeep":
-                camHeight = player.dimZ * 1.3
-            elif carName == "racecar":
-                camHeight = player.dimZ * 1.7
-            else:
-                camHeight = player.dimZ * 1.7
-
-                #1.3 is very forward
-
+        camHeight = player.dimZ * 1.7
         xOffset = camDistance * math.sin(theta)
         yOffset = -camDistance * math.cos(theta)
 
         self.camera.setPos(x + xOffset, y + yOffset, z + camHeight)
 
-        # Look forward a bit
-        # Remember to calculate the perspective offset accordingly
         if "perspective" in self.camConfig:
             perspectiveOffset = 10
             xOffset = perspectiveOffset * math.sin(-theta)
             yOffset = perspectiveOffset * math.cos(-theta)
             self.camera.lookAt(x + xOffset, y + yOffset, z)
 
-        # Look at center of car
         if "_rotate" in self.camConfig:
             self.camera.lookAt(x, y, z)
 
@@ -561,14 +268,9 @@ class RacingGame(Game):
     def loadModels(self):
         self.cars = []
         Racecar.nRacecars = 0
-
         self.racetrack = Racetrack(self, Game.selectedTrack)
-
-        # Only the positions are updated here because we want to space them out
-        # But car facing and checkpoint handling are handled inside the init function
         self.player = Racecar(self, Game.selectedCar, Game.selectedPassenger, self.render)
         self.cars.append(self.player)
-
         if self.printStatements: print(f"Opponent cars generated with difficulty {Game.level}")
 
     # Key Events
@@ -576,13 +278,10 @@ class RacingGame(Game):
         # Create a function to key maps
         # "<function>": [ <list of key ids> ]
         functionToKeys = {
-            "forward": [ "arrow_up", "w" ],
-            "backward": [ "arrow_down", "s" ],
-            "turnLeft": [ "arrow_left", "a" ],
-            "turnRight": [ "arrow_right", "d" ],
-            "camConfigRotate": [ "v" ],
-            "camConfigBehind": [ "c" ],
-            "drifting": [ "space", "z" ]
+            "forward": [ "arrow_up" ],
+            "backward": [ "arrow_down"],
+            "turnLeft": [ "arrow_left"],
+            "turnRight": [ "arrow_right" ],
         }
 
         for fn in functionToKeys:
@@ -594,42 +293,12 @@ class RacingGame(Game):
             for key in keys:
                 # Key Down
                 self.accept(key, self.setKeyDown, [fn, 1])
-
                 # Key Up
                 self.accept(key+"-up", self.setKeyDown, [fn, -1])
 
-        # Only need the key-release events
-        # (fn, key list, args)
-        keyReleaseMap = [
-            (self.setCameraView, ["1"], ["perspective"]),
-            (self.setCameraView, ["2"], ["birdsEye"]),
-            (self.setCameraView, ["3"], ["firstPerson"]),
-            (self.restartGame, ["r"], None),
-            (self.oobe, ["="], None),
-            (self.togglePause, ["backspace"], [False]),
-            (self.togglePause, ["p", "escape"], None),
-            (self.togglePrintStatements, ["\\"], None),
-            (self.toggleMute, ["m"], None)
-        ]
-
-        for fn, keys, args in keyReleaseMap:
-            for key in keys:
-                if isinstance(args, list) and len(args) > 0:
-                    self.accept(key+"-up", fn, args)
-                else:
-                    self.accept(key+"-up", fn)
 
     def setKeyDown(self, key, value):
-        # In order to account for multiple keys
-        # mapped to the same function
-        # We increment isKeyDown by 1 if key down
-        # and decrement if key up
-        # This way even if another key is held down
-        # the function still runs
         self.isKeyDown[key] += value
-
-        # Make sure we don't go
-        # below 0 for any reason
         if self.isKeyDown[key] < 0:
             self.isKeyDown[key] = 0
 
@@ -642,18 +311,10 @@ class RacingGame(Game):
         if self.printStatements: print("Camera view set to: " + self.camConfig)
 
     def keyPressHandler(self, task):
-        # NOTE: In order to allow for diagonal movement
-        #       we cannot use elif
-
         if self.paused or self.isGameOver:
             return Task.cont
 
         player = self.player
-
-        if self.isKeyDown["drifting"] > 0:
-            player.drifting = True
-        else:
-            player.drifting = False
 
         if self.isKeyDown["forward"] > 0:
             player.doDrive("forward")
@@ -667,15 +328,6 @@ class RacingGame(Game):
         if self.isKeyDown["turnRight"] > 0:
             player.doTurn("right")
 
-        if self.isKeyDown["camConfigRotate"] > 0:
-            self.camConfig += "_rotate"
-        else:
-            self.camConfig = self.camConfig.replace("_rotate", "")
-
-        if self.isKeyDown["camConfigBehind"] > 0:
-            self.camConfig += "_behind"
-        else:
-            self.camConfig = self.camConfig.replace("_behind", "")
 
         return Task.cont
 
--- Terrain.py
+++ Terrain.py
@@ -22,23 +22,7 @@ class Ground(Obj3D):
 
         # Make bottom extra big
         padZ = 10
-        '''
-        colBox = CollisionBox(
-            # center is padZ below the floor
-            (self.relOffsetX, self.relOffsetY, self.relOffsetZ - padZ),
-            # dx, dy, dz
-            self.relDimX/2, self.relDimY/2, self.relDimZ/2 + padZ
-        )
-        colPlane = CollisionPlane(
-            Plane(Vec3(0, 0, 1), Point3(self.relativeOffset))
-        )
 
-        colNode = self.createIsolatedCollisionObj(
-            "floor", colPlane, 
-            fromBitmask=self.gameObj.colBitMask["off"], intoBitmask=self.gameObj.colBitMask["floor"],
-            show = True
-        )
-        '''
 
 class Terrain():
     def __init__(self, gameObj):
@@ -46,27 +30,9 @@ class Terrain():
 
         racetrack = self.gameObj.racetrack
 
-        # Load infinitely extending floor
-        '''
-        planeSolid = CollisionPlane(
-            Plane(Vec3(0, 0, 1), Point3(0, 0, 0))
-        )
-
-        floorPlane = Obj3D.createIsolatedCollisionObj(
-            "floor", planeSolid, intoBitmask=self.gameObj.colBitMask["floor"]
-        )
-        '''
-
-        # Load ground
-        #self.ground = Ground(self.gameObj, "ground")
-        #self.ground.setScale(scaleX=1.5, scaleY=1.5)
-
         _, angles = racetrack.leftTrackPoints[0]
         pos = racetrack.points[0]
 
         self.startLine = Ground(self.gameObj, "cornfield", pos=pos)
         self.startLine.rotate(
             dh=angles[0], dp=angles[1])
-
-        # Sky
-        #self.sky = Obj3D("FarmSky")
